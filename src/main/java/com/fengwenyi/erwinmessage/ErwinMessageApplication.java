e504527f16413bd8720b100fc386c9fd94e86e0e
冯文议 <xfsy_2015@163.com>
2021-07-22 17:09:20 +0800

    build: 构建工程

---
+++ src/main/java/com/fengwenyi/erwinmessage/ErwinMessageApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.erwinmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErwinMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErwinMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengwenyi/erwinmessage/ErwinMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.erwinmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErwinMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
